ea538700964aeab138f8288bb7de6c12da7483ff
Small Boat <2226052035@q.com>
2023-12-14 16:15:10 +0800

    测试后端Bot的增删改查

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -6,10 +6,84 @@
 
 <script>
 import ContentField from '../../../components/ContentField.vue'
+import $ from 'jquery'
+import {useStore} from "vuex";
 
 export default {
   components: {
     ContentField
+  },
+  setup() {
+    const store = useStore();
+    // $.ajax({
+    //   url: "http://127.0.0.1:8090/user/bot/add/",
+    //   type: "post",
+    //   data: {
+    //     title: "Bot标题222",
+    //     description: "Bot描述",
+    //     content: "111",
+    //   },
+    //   headers: {
+    //     Authorization: "Bearer " + store.state.user.token,
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp)
+    //   }
+    // })
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:8090/user/bot/delete/",
+    //   type: "post",
+    //   data: {
+    //     botId: "3"
+    //   },
+    //   headers: {
+    //     Authorization: "Bearer " + store.state.user.token,
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp)
+    //   }
+    // })
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:8090/user/bot/update/",
+    //   type: "post",
+    //   data: {
+    //     botId: "4",
+    //     title: "Bot标题aa",
+    //     description: "Bot描述aa",
+    //     content: "111a",
+    //   },
+    //   headers: {
+    //     Authorization: "Bearer " + store.state.user.token,
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp)
+    //   }
+    // })
+
+    $.ajax({
+      url: "http://127.0.0.1:8090/user/bot/getList/",
+      type: "get",
+      headers: {
+        Authorization: "Bearer " + store.state.user.token,
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp)
+      }
+    })
   }
 }
 </script>
